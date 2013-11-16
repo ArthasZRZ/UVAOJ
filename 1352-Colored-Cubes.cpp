c9f935e5b32b33bec04d019c4533c458e6edcfb3
NaN3 <rz940303@gmail.com>
2013-11-16 20:00:23 +0800

    2013-11-16

---
+++ 1352-Colored-Cubes.cpp
@@ -0,0 +1,19 @@
+// There are two parts of this question
+// 1: how to implement equal
+// -> use some locate method
+// 2: how to emumerate the answer?
+// -> set a hash from color to id
+// -> and an array to calculate the total number of cubes in the
+// same color
+// -> ******************
+
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+
+	return 0;
+}
